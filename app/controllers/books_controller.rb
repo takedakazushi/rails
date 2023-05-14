f86179c0a510b48ea8c7f0952c09015a8eddfd5e
takedakazushi <ec2-user@ip-172-31-44-28.ap-northeast-1.compute.internal>
2023-05-14 13:14:35 +0000

    3-1~11

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,39 +5,41 @@ class BooksController < ApplicationController
   end
   def show
     @books=Book.all
+    @book=Book.find(params[:id])
   end
   def edit
     @books=Book.all
+    @book=Book.find(params[:id])
+  end
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   def new
-    @book = Book.all
+    @book = Book.new
   end
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "投稿に成功しました"
       redirect_to book_path(@book.id)
     else
-      render :new
+      flash[:alret] = "投稿に失敗しました"
+      @books=Book.all
+      render :index
     end
-    # 1.&2.
-    book = Book.new(book_params)
-    # 3 .
-    book.save
-    # 4.
-    redirect_to '/top'
-  end
-  private
-  def book_params
-    params.require(:book).permmit(:title, :body)
+
   end
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to 'books'
+    redirect_to '/books'
   end
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,4 @@ class Book < ApplicationRecord
   has_one_attached :image
   validates :title, presence: true
   validates :body, presence: true
-  validates :image, presence: true
 end
